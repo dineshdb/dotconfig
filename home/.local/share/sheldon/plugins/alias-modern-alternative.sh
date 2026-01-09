7dbc1108e08cb0bd7cd15d02af8c686a09e980bb
Dinesh Bhattarai <dineshdb@dbhattarai.info.np>
2026-01-10 00:17:42 +0545

    Clean up modern alternatives in alias script
    Removed outdated modern alternatives for commands.

--- home/.local/share/sheldon/plugins/alias-modern-alternative.sh
+++ home/.local/share/sheldon/plugins/alias-modern-alternative.sh
@@ -13,11 +13,4 @@ modern_alternative vi           nvim
 modern_alternative man          info
 modern_alternative top          htop
 modern_alternative gradlew      gw
-modern_alternative man          batman.sh
-modern_alternative diff         delta
-modern_alternative grep         rg
-modern_alternative time         hyperfine
-modern_alternative ls			lsd
-modern_alternative cp			xcp
-modern_alternative du			dust
-modern_alternative find			fd
+
